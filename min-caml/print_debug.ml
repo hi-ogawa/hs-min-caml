a692abe8b78773dbd5da9220641a1b6afb67b4d1
hiroshi ogawa <hi.ogawa.zz@gmail.com>
2012-10-06 16:14:36 +0900

    OK to print debugs Syntax Form.

--- min-caml/print_debug.ml
+++ min-caml/print_debug.ml
@@ -1,42 +1,74 @@
 open Printf
 open Syntax
 
+(* Type.t -> string *)
+let rec tyToStr ty = 
+  match ty with
+  | Type.Unit -> "unit" | Type.Bool -> "bool" | Type.Int -> "int" | Type.Float -> "float"
+  | Type.Fun (ts, t) -> "#fun " ^ "[args: " ^ (String.concat ", " (List.map (fun t1 -> tyToStr t1) ts)) ^ "], " ^ "[result: " ^ (tyToStr t) ^ "]"
+  | Type.Tuple ts -> "#tuple [" ^ (String.concat ", " (List.map (fun t1 -> tyToStr t1) ts)) ^ "]"
+  | Type.Array t -> "#array [" ^ (tyToStr t) ^ "]"
+  | Type.Var r -> 
+    (match !r with
+      | None -> "tvar: None"
+      | Some sty -> "tvar: [" ^ (tyToStr sty) ^ "]")
 
-let type_to_string ty = "??"
+(* (Id.t * Type.t) list -> string *)
+let rec argsToStr args =
+  String.concat ", " (List.map (fun (x,t) -> x ^ " (" ^ (tyToStr t) ^ ")") args)
 
-let rec print_syntax exp d () =
-  for l = 1 to d do print_string "\t"; done;
-match exp with
-      | Unit	-> printf "-UNIT- ()\n"
-      | Bool(b)	-> printf "-BOOL- %B\n" b
-      | Int(i)	-> printf "-INT- %d\n" i
-      | Float(f)	-> printf "-FLOAT- %f\n" f
-      | Not(e)	-> printf "<NOT>\n"; print_syntax e (d+1) ()
-      | Neg(e)	-> printf "<NEG>\n"; print_syntax e (d+1) ()
-      | Add(e1, e2)	-> printf "<ADD>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | Sub(e1, e2)	-> printf "<SUB>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | FNeg(e)	-> printf "<FNEG>\n"; print_syntax e (d+1) ()
-      | FAdd(e1, e2)	-> printf "<FADD>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | FSub(e1, e2)	-> printf "<FSUB>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | FMul(e1, e2)	-> printf "<FMUL>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | FDiv(e1, e2)	-> printf "<FDIV>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | Eq(e1, e2)	-> printf "<EQ>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | LE(e1, e2)	-> printf "<LE>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | If(e1, e2, e3)	-> printf "<IF>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | Let((x, t), e1, e2)	-> printf "<LET> var: %s, type: %s\n" x (type_to_string t); print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | Var(x)	-> printf "-VAR- var: %s\n" x
-      | LetRec({name = (x,t); args = yts; body = e1}, e2)	-> printf "<LETREC> func: %s, type: %s\n" x (type_to_string t); print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | App(e, es)	-> printf "<APP>\n"; print_syntax e (d+1) (); List.iter (fun e -> print_syntax e (d+1) ()) es
-      | Tuple(es)	-> printf "<TUPLE>\n"; List.iter (fun e -> print_syntax e (d+1) ()) es
-      | LetTuple(xts, e1, e2)	-> printf "<LETTUPLE>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | Array(e1, e2)	-> printf "<ARRAY>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | Get(e1, e2)	-> printf "<GET>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) ()
-      | Put(e1, e2, e3)	-> printf "<PUT>\n"; print_syntax e1 (d+1) (); print_syntax e2 (d+1) (); print_syntax e3 (d+1) ()
+(* int -> string -> unit *)
+let rec p_d depth str =
+  for l = 1 to depth do print_string "    "; done;
+  print_string str
 
+(* Syntax.t -> string *)
+let rec synToStr exp =
+  match exp with
+    | Unit -> "-UNIT-: ()"
+    | Bool(b) -> sprintf "-BOOL-: %B" b
+    | Int(i)  -> sprintf "-INT-: %i" i
+    | Float(f) -> sprintf "-FLOAT-: %f" f
+    | Var(x) -> sprintf "-VAR-: %s" x
+    | Not _ -> "<NOT>"| Neg _ -> "<Neg>" | FNeg _ -> "<FNeg>"
+    | Add _ -> "<ADD>" | Sub _ -> "<SUB>" | FAdd _ -> "<FADD>"
+    | FSub _ -> "<FSub>" | FMul _ -> "<FMUL>" | FDiv _ -> "<FDIV>"
+    | Eq _ -> "<EQ>" | LE _ -> "<LE>" | Array _ -> "<ARRAY>" | Get _ -> "<GET>"
+    | If _ -> "<IF>" | Put _ -> "<PUT>"
+    | Let ((x,t),_,_) -> sprintf "<LET>: [%s (%s)]" x (tyToStr t)
+    | LetRec ({name = (x,t); args = yts; body = e1}, e2) -> sprintf "<LETREC>: [%s (%s)], [args: %s]" x (tyToStr t) (argsToStr yts)
+    | App _ -> "<APP>"
+    | Tuple _ -> "<TUPLE>" 
+    | LetTuple (xts,_,_) -> sprintf "<LETTUPLE>: [binds: %s]" (argsToStr xts)
+
+(* Syntax.t -> int -> unit *)
+let rec p_syn exp d =
+  p_d d "";
+  printf "%s\n" (synToStr exp);
+  (match exp with
+    | Unit | Bool _ | Int _ | Float _ | Var _
+      -> ()
+    | Not(e) | Neg(e) | FNeg(e)
+      -> p_syn e (d+1)
+    | Add(e1, e2) | Sub(e1, e2)	| FAdd(e1, e2) | FSub(e1, e2) | FMul(e1, e2)
+    | FDiv(e1, e2) | Eq(e1, e2) | LE(e1, e2) | Array(e1, e2) | Get(e1, e2)
+      -> p_syn e1 (d+1) ; p_syn e2 (d+1)
+    | If(e1, e2, e3) | Put(e1, e2, e3)
+      -> p_syn e1 (d+1); p_syn e2 (d+1); p_syn e3 (d+1)
+    | Let(_, e1, e2)
+      -> p_syn e1 (d+1) ; p_syn e2 (d+1) 
+    | LetRec({body = e1}, e2)
+      -> p_syn e1 (d+1) ; p_syn e2 (d+1)
+    | App(e, es)
+      -> p_syn e (d+1) ; List.iter (fun e -> p_syn e (d+1) ) es
+    | Tuple(es)
+      -> List.iter (fun e -> p_syn e (d+1) ) es
+    | LetTuple(xts, e1, e2)
+      -> p_syn e1 (d+1) ; p_syn e2 (d+1))
 
 let debug1 filename =
   let inchan = open_in (filename) in
   try
-    print_syntax (Parser.exp Lexer.token (Lexing.from_channel inchan)) 0 ()
+    p_syn (Typing.f (Parser.exp Lexer.token (Lexing.from_channel inchan))) 0;
   with e -> (close_in inchan; raise e)
 
